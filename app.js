5a7a2d2cef9dd767252ec7063f1ba5397a95786a
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-06-16 13:02:26 +0900

    Set up webpack

--- app.js
+++ app.js
@@ -1,104 +1,34 @@
-// import { getData, capitalize, display, getEl} from './modules/util.js'
-const Pokedex = require("pokeapi-js-wrapper")
-const P = new Pokedex.Pokedex()
-// console.log(P);
+const printHello = require('./print-hello');
 
-// with await, be sure to be in an async function (and in a try/catch)
-// (async () => {
-//     const golduck = await P.getPokemonByName("golduck")
-//     console.log(golduck)
-//   })()
+printHello();
 
-let name = "charizard"
 
-// name, types
 
-// or with Promises
-P.getPokemonByName(name)
-  .then(function(response) {
-    type1 = response.types[0].type.name
-    type2 = response.types[1].type.name
+
+
+
+// // import { getData, capitalize, display, getEl} from './modules/util.js'
+// const Pokedex = require("pokeapi-js-wrapper")
+// const P = new Pokedex.Pokedex()
+
+// // with await, be sure to be in an async function (and in a try/catch)
+// // (async () => {
+// //     const golduck = await P.getPokemonByName("golduck")
+// //     console.log(golduck)
+// //   })()
+
+// let name = "charizard"
+
+// // name, types
+
+// // or with Promises
+// P.getPokemonByName(name)
+//   .then(function(response) {
+//     type1 = response.types[0].type.name
+//     type2 = response.types[1].type.name
   
-    console.log(type1)
-    console.log(type2)
-  })
+//     console.log(type1)
+//     console.log(type2)
+//   })
 
 
-
-// getLocal -> getJSON -> onLoad -> displayData
-
-// // 
-// // Retrieve Local Storage
-// // 
-
-// window.addEventListener("DOMContentLoaded", function(e) {
-//     // let charName = localStorage.getItem("characterName");
-//     // let charClass = localStorage.getItem("characterClass");
-//     // let charRace = localStorage.getItem("characterRace");
-//     // let charElement = localStorage.getItem("characterElement");
-
-//     // let charChoices = [charName, charClass, charRace, charElement];
-
-//     let localData = '';
-
-//     // Begins the process of requesting the JSON data.
-//     getData('data.json', onLoad, localData)
-// })
-
-// getData('data.json', onLoad);
-
-// // 
-// // On Load
-// // 
-
-// function onLoad(data, localData) {
-//     // displayCards(data);
-//     displayData(data);
-// }
-
-// // 
-// // Display Data
-// // 
-
-// function displayData(data) {
-//     const target = getEl('test');
-//     let charClasses = data.charClasses;
-
-//     let content = '';
-//     // Content Open
-//     content += `<div class="album py-5 bg-light">
-//                     <div class="row">`
-
-//     // Class Cards
-//     for (let i = 0; i < charClasses.length; i++) {
-//         const curClass = charClasses[i];
-//         const reqStats = curClass.reqStats;
-//         const statGains = curClass.statGains;
-//         const name = capitalize(curClass.name);
-//         let image;
-//         if (curClass.images[0]) {
-//             image = curClass.images[0];
-//         }
-//         else {
-//             image = curClass.images[1];
-//         }
-        
-//         content += `<div class="col-6 col-sm-3 col-md-2">
-//                         <div class="card">
-//                             <div class="row">
-//                                 <img src="${image}" class="img-fluid" alt="..."></img>
-//                                 <h3>${name}</h3>
-//                                 <p>Required Stats: ${reqStats}
-//                                 <p>Required Stats: ${statGains}
-//                             </div>
-//                         </div>
-//                     </div>`;
-//     }
-
-//     // Content Close
-//     content += `</div>
-//                     </div>`
-
-//     // Display Content
-//     display(target, content);
-// }
--- modules/getEl.js
+++
@@ -1,6 +0,0 @@
-// Get HTML element by id
-function getEl(id) {
-    return document.getElementById(id);
-}
-
-export default getEl
