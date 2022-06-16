223cb09ef8d97cf4c6b3c799fa545621c77cc282
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-06-16 13:09:35 +0900

    Set up pokeapi js-wrapper to work with webpack

--- app.js
+++ app.js
@@ -1,34 +1,23 @@
-const printHello = require('./print-hello');
+// import { getData, capitalize, display, getEl} from './modules/util.js'
+const Pokedex = require("pokeapi-js-wrapper")
+const P = new Pokedex.Pokedex()
 
-printHello();
+// with await, be sure to be in an async function (and in a try/catch)
+// (async () => {
+//     const golduck = await P.getPokemonByName("golduck")
+//     console.log(golduck)
+//   })()
 
+let name = "charizard"
 
+// name, types
 
-
-
-
-// // import { getData, capitalize, display, getEl} from './modules/util.js'
-// const Pokedex = require("pokeapi-js-wrapper")
-// const P = new Pokedex.Pokedex()
-
-// // with await, be sure to be in an async function (and in a try/catch)
-// // (async () => {
-// //     const golduck = await P.getPokemonByName("golduck")
-// //     console.log(golduck)
-// //   })()
-
-// let name = "charizard"
-
-// // name, types
-
-// // or with Promises
-// P.getPokemonByName(name)
-//   .then(function(response) {
-//     type1 = response.types[0].type.name
-//     type2 = response.types[1].type.name
+// or with Promises
+P.getPokemonByName(name)
+  .then(function(response) {
+    type1 = response.types[0].type.name
+    type2 = response.types[1].type.name
   
-//     console.log(type1)
-//     console.log(type2)
-//   })
-
-
+    console.log(type1)
+    console.log(type2)
+  })
