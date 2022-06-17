b07110cb5e40070e6ec63b8fa59029e7e20f9a32
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-06-17 12:14:57 +0900

    Set up basic functionality to display name, types, and image of specified Pokemon

--- app.js
+++ app.js
@@ -12,24 +12,11 @@ const display = util.display;
 console.log(capitalize('adsadfs yoyoyo'));
 
 const root = getEl('root');
-const message = capitalize('hello friend');
-display(root, message)
+// const message = capitalize('hello friend');
+// display(root, message)
 
 
-let newPokemon = getPokemon('charizard');
-// console.log(newPokemon);
-
-
-// with await, be sure to be in an async function (and in a try/catch)
-// (async () => {
-//     const golduck = await P.getPokemonByName("golduck")
-//     console.log(golduck)
-//   })()
-
-
-// name, types
-
-// or with Promises
+// let newPokemon = getPokemon('charizard');
 
 function getTypes(pokemon) {
   // Retrieve Pokemon data from api by name
@@ -48,3 +35,34 @@ function getTypes(pokemon) {
 }
 
 getTypes("bulbasaur");
+
+
+
+function displayPokemon(pokemon) {
+  // Retrieve Pokemon data from api by name
+  P.getPokemonByName(pokemon)
+  .then(function(response) {
+    const name = response.name;
+    let typesData = response.types;
+    let types = [];
+    typesData.forEach(element => {
+      let currentType = element.type.name;
+      types.push(currentType)
+    });
+    const image = response.sprites.front_default
+
+    let content = '';
+    content += `<image src="${image}">`
+    content += `<p>Name: ${name}</p>`
+    content += `<p>Type: ${types}</p>`
+    
+    display(root, content);
+
+    console.log(response);
+    
+    console.log(types);
+    return types;
+  })
+}
+
+displayPokemon('charizard')
