346025682d9c84f9265307ad238876b257616166
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-08-03 10:11:12 +0900

    Set up code to make sure data for all Pokemon is returned in order before displaying id and name to the screen.

--- app.js
+++ app.js
@@ -12,87 +12,44 @@ const log = util.log;
 const root = getEl('root');
 const displayPokemon = require('./modules/pokemon/displayPokemon');
 
-// Get Pokemon by name:
-// P.getPokemonByName(pokemon).then(function(response) {
-//    return response 
-// });
-
-// Get a Pokemon's type info:
-// P.getTypeByName(type).then(function(response) {
-  //  return response;
-// });
-
-// P.getPokemonSpeciesList
-
-const getPokeSprite = (name) => {
-  P.getPokemonByName(name).
-    then((response) => {
-      const image = response.sprites.front_default
-      console.log('image:')
-      console.log(image);
-    })
-    // const image = pokemon.sprites.front_default
-}
-
 //
 // Get array of all Pokemon (name and url)
 // 
+
 const interval = {
+  // Where to start (0 = beginning of Pokemon list (Bulbasaur))
   offset: 0,
+  // How man Pokemon to return (905 is max number pre Scarlet and Violet)
   limit: 905,
 }
-console.log('get pokemon list')
-P.getPokemonsList(interval).then(function(response) {
-  const allPokemon = response.results;
-  console.log('all Pokemon:')
-  console.log(allPokemon)
-  const root = document.getElementById('root');
-  let content = '';
-  for (let i = 0; i < 151; i++) {
-    const pokemon = allPokemon[i].name;
-    P.getPokemonByName(pokemon).
-    then((response) => {
-      const image = response.sprites.front_default
-      console.log('image:')
-      console.log(image);
-      content += `<image src="${image}">`
+// Returns an array of all pokemon with name and url
+P.getPokemonsList(interval).
+  then((response) => {
+    const allPokemon = response.results;
+    const root = document.getElementById('root');
+    let content = '';
+
+    const promises = [];
+
+    for (let i = 0; i < 151; i++) {
+      const pokemon = allPokemon[i].name;
+      promises.push(P.getPokemonByName(pokemon))
+    }
+
+    // returns results once every promise is fulfilled. Makes sure data for each Pokemon is returned and that it is in the correct order
+    Promise.all(promises)
+      .then( results => {
+        // Creates an array of data for all Pokemon
+        const pokemon = results.map(result => {
+          const id = result.id;
+          const name = result.name;
+          const image = result.sprites.front_default;
+          // types = result.types.map((type) => type);
+          content += `<p>#${id} ${name}</p>`
+        });
+        
+        // Display Pokemon data to root element
+        root.innerHTML = content;
     })
-  }
-  setTimeout(() => {
-    root.innerHTML = content;
-  }, 2000)
   
 })
-
-
-
-
-
-// console.log('Pokedex:')
-// console.log(P)
-
-
-
-
-// // function getTypeData(type) {
-// //   P.getTypeByName(type).then(function(response) {
-// //     console.log('Type Info: ')
-// //     console.log(response)
-// //   })
-// // }
-
-// console.log('TRY TO GET TYPE:')
-// getType('fire');
-
-
-
-// // Get Pokemon Data
-// P.getPokemonByName('pikachu').
-//   then((response) => {
-//     console.log(`${response.name}'s info retrieved.`)
-//     console.log(response)
-//     // Display Pokemon data
-//     getTypes(response);
-//     displayPokemon(response);
-//   })
-//   .catch((err) => console.log('Could not retrieve Pokemon data from Poke API.', err));
