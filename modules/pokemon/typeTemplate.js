d7b2bd0b89804f76e5f0da49fd2a9545370def9a
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-09-01 12:49:43 +0900

    Capitalize type names in HTML generated by JavaScript

--- modules/pokemon/typeTemplate.js
+++ modules/pokemon/typeTemplate.js
@@ -1,1 +1,6 @@
-module.exports = (type) => `<li class="shadow type rounded text-center ${type} p-1 m-1">${type}</li>`
+const U = require('../util/util');
+
+module.exports = (type) => {
+    let cType = U.capitalize(type);
+    return `<li class="shadow type rounded text-center ${type} p-1 m-1">${cType}</li>`
+}
