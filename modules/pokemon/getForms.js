8eb6b32292c152d74bb85cbf376a8e20c413c523
Akamarak <67317533+Mark-David-Hill@users.noreply.github.com>
2022-09-29 14:38:25 +0900

    Filter out forms I don't want to be displayed for Pokemon #1-151

--- modules/pokemon/getForms.js
+++ modules/pokemon/getForms.js
@@ -3,9 +3,27 @@ const P = new Pokedex.Pokedex()
 
 module.exports = async (pokemon) => {
     let forms = [];
+    // console.log('pokemon::')
+    // console.log(pokemon)
     formData = await P.getPokemonSpeciesByName(pokemon).then((response) => {
         response.varieties.forEach(pokeData => {
-            forms.push(pokeData.pokemon.name);
+            let formName = pokeData.pokemon.name;
+            // forms.push(formName);
+            if (!formName.includes('totem')) {
+                if (pokemon === 'pikachu') {
+                    if (formName === 'pikachu' || formName === 'pikachu-gmax') {
+                        forms.push(formName);
+                    }
+                }
+                else if (pokemon === 'eevee') {
+                    if (formName !== 'eevee-starter') {
+                        forms.push(formName);
+                    }
+                }
+                else {
+                    forms.push(formName);
+                }
+            }
         });
     })    
     return forms;
